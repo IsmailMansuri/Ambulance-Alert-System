8b72a0739adf31ac83e54a49045830888b20f08c
Ismail M <isimansuri@gmail.com>
2020-01-26 20:31:42 -0500

    testing

--- One Byte/Receive/Receive.ino
+++ One Byte/Receive/Receive.ino
@@ -1,16 +1,21 @@
 //Receiver program
 #include <SPI.h>
 #include "nRF24L01.h"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> 
 
 int led_pin=2;
 
 Nrf24l Mirf = Nrf24l(10, 9);
-
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 byte value;
 
 void setup()
 {
   pinMode(led_pin,OUTPUT);
+
+  lcd.init();
+  
   
   Serial.begin(9600);
   Mirf.spi = &MirfHardwareSpi;
@@ -25,6 +30,8 @@ void setup()
 void loop()
 {
 
+  
+
   digitalWrite(led_pin,HIGH);
   delay(250);
   digitalWrite(led_pin,LOW);
@@ -34,5 +41,24 @@ void loop()
     Mirf.getData(&value);
     Serial.print("Got data: ");
     Serial.println(value);
+
+    if (value == 255){
+       lcd.backlight();
+       lcd.setCursor(3, 0); 
+       lcd.print("Ambulance  "); 
+       lcd.setCursor(4, 1); 
+       lcd.print("Inbound");
+       delay(1500);
+       lcd.clear();
+       lcd.setCursor(3, 0); 
+       lcd.print("Pull Over"); 
+       delay(1500);
+    }
   }
+  else{
+      lcd.clear();
+      lcd.noBacklight();
+      value=0;
+  }
+  
 }
