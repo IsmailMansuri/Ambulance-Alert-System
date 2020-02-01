fa18f4a3abeaf79cd69afd92f86c99ce6b953a53
Ismail M <isimansuri@gmail.com>
2020-02-01 09:34:33 -0500

    added functions

--- Device Code/Client/Receive.ino
+++ Device Code/Client/Receive.ino
@@ -6,8 +6,8 @@
 
 int led_pin=2;
 
-Nrf24l Mirf = Nrf24l(10, 9);
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+Nrf24l Mirf = Nrf24l(10, 9);                             //Set CE, CSN pins
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);  //Setup 16x2 LCD
 byte value;
 
 void setup()
@@ -19,46 +19,53 @@ void setup()
   
   Serial.begin(9600);
   Mirf.spi = &MirfHardwareSpi;
-  Mirf.init();
-  Mirf.setRADDR((byte *)"FGHIJ"); //Set your own address (receiver address) using 5 characters
+  Mirf.init();                                          //Initialize radio
+  Mirf.setRADDR((byte *)"FGHIJ");                       //Set receiver address
   Mirf.payload = sizeof(value);
-  Mirf.channel = 90;             //Set the used channel
+  Mirf.channel = 90;                                    //Set the used channel
   Mirf.config();
-  Serial.println("Listening...");  //Start listening to received data
+  Serial.println("Listening...");                       //Start listening to received data
 }
 
 void loop()
 {
+  receive();
+}
 
-  
-
-  digitalWrite(led_pin,HIGH);
-  delay(250);
-  digitalWrite(led_pin,LOW);
-  delay(250);
-  
-  if (Mirf.dataReady()) { //When the program is received, the received data is output from the serial port
+void receive()
+{
+  if (Mirf.dataReady()){                                //If received value is 255 display to LCD, else clear the LCD
     Mirf.getData(&value);
     Serial.print("Got data: ");
     Serial.println(value);
 
     if (value == 255){
-       lcd.backlight();
-       lcd.setCursor(3, 0); 
-       lcd.print("Ambulance  "); 
-       lcd.setCursor(4, 1); 
-       lcd.print("Inbound");
-       delay(1500);
-       lcd.clear();
-       lcd.setCursor(3, 0); 
-       lcd.print("Pull Over"); 
-       delay(1500);
+      display(); 
     }
   }
   else{
-      lcd.clear();
-      lcd.noBacklight();
-      value=0;
+    clearLCD();
   }
-  
+
+}
+
+void display()                                         //Display message on LCD display
+{
+  lcd.backlight();
+  lcd.setCursor(3, 0); 
+  lcd.print("Ambulance  "); 
+  lcd.setCursor(4, 1); 
+  lcd.print("Inbound");
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(3, 0); 
+  lcd.print("Pull Over"); 
+  delay(1500);
+}
+
+void clearLCD()
+{
+  lcd.clear();
+  lcd.noBacklight();
+  value=0;
 }
--- Device Code/Server/Emitter.ino
+++ Device Code/Server/Emitter.ino
@@ -3,27 +3,31 @@
 #include <SPI.h>
 #include "nRF24L01.h"
 
-Nrf24l Mirf = Nrf24l(10, 9);
-byte value;
+Nrf24l Mirf = Nrf24l(10, 9);                //Set CE, CSN pins
+byte value = 255;                           //Set send value 255
 
 void setup()
 {
   Serial.begin(9600);
   Mirf.spi = &MirfHardwareSpi;
-  Mirf.init();
-  Mirf.setRADDR((byte *)"ABCDE");           //Set your own address (sender address) using 5 characters
+  Mirf.init();                              //Initialize radio
+  Mirf.setRADDR((byte *)"ABCDE");           //Set sender address
   Mirf.payload = sizeof(value);
-  Mirf.channel = 90;              //Set the channel used
+  Mirf.channel = 90;                        //Set the channel used
   Mirf.config();
 }
 
 void loop()
 {
-  Mirf.setTADDR((byte *)"FGHIJ");           //Set the receiver address
-  value = 255;                      //Send value 255
-  Mirf.send(&value);                //Send instructions, send random number value
+  transmit();
+}
+
+void transmit()
+{
+  Mirf.setTADDR((byte *)"FGHIJ");           //Set the receiver address                     
+  Mirf.send(&value);                        //Send value
   Serial.print("Wait for sending.....");
-  while (Mirf.isSending())//Until you send successfully, exit the loop
+  while (Mirf.isSending())                  //Until send success, exit the loop
   delay(1);        
   Serial.print("Send success:");
   Serial.println(value);
